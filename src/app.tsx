7f5b6e3318c618b1f4cc419b3a20ea7997c437e5
bhenrique16 <bh.henrique16@gmail.com>
2024-02-10 22:27:41 -0300

    Add note creation functionality and display existing notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
 export function App() {
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+    const notesArray = ([newNote, ...notes])
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -19,18 +43,10 @@ export function App() {
       <div className="h-px bg-stone-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: 'Hello'
-          }} />
-
-
-
-
-
-
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
 
       </div>
     </div>
